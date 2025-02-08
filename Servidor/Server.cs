6b48c080df5b3e5af0c886ec8c87d45fa6970394
uo287594 <uo287594@uniovi.es>
2025-02-08 17:30:29 +0100

    Actualizació del servidor

--- Servidor/Server.cs
+++ Servidor/Server.cs
@@ -2,26 +2,75 @@
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using Vocabulario;
 
-namespace ServidorUDP
+namespace Servidor
 {
-    class Server
+    public class server
     {
-        public void Run()
+        private const int port = 50000;
+
+        private static void Run()
         {
-            UdpClient udpServer = new UdpClient(50000);
-            IPEndPoint remoteEP = new IPEndPoint(IPAddress.Any, 0);
+            // Inicializamos el puerto UDP y la dirección IP
+            UdpClient cliente = new UdpClient(port);
+            IPEndPoint ip = new IPEndPoint(IPAddress.Any, port);
+            bool conexion = false;
 
-            Console.WriteLine("Servidor UDP esperando conexión en el puerto 50000...");
+            // Inicializamos la variable para el contador.
+            int cont = 0;
 
-            // Aquí se debe desarrollar la recepción y guardado de los mensajes.
+            // Creamos una instancia try-catch para manejar excepciones
 
+            try
+            {
+                // Bucle infinito para realizar la recepción de mensajes por parte del cliente.
+                while (true)
+                {
+
+                    if (cont == 0)
+                    {
+                        Console.WriteLine("Esperando conexión con el cliente...");
+                        cont++;
+                    }
+
+                    // Debemos recibir el mensaje por parte del cliente.
+                    byte[] bytes = cliente.Receive(ref ip);
+
+                    // Implementamos verificación de conexión.
+                    if (!conexion)
+                    {
+                        Console.WriteLine("Conexión establecida con el cliente.");
+                        conexion = true;
+                    }
+
+                    // Debemos de convertir los datos recibidos a un string decodificandolos.
+                    Datos msg = new Datos();
+                    msg.Decode(bytes);
+
+                    // Guardaremos los datos recibidos en un archivo de texto.
+                    using (System.IO.StreamWriter file = new System.IO.StreamWriter("datos.txt", true))
+                    {
+                        file.WriteLine("Secuencia: " + msg.seq + " Número: " + msg.num);
+                    }
+                }
+            }
+
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+
+            finally
+            {
+                Console.WriteLine("Conexión finalizada.");
+                cliente.Close();
+            }
         }
 
-        static void Main()
+        public static void Main()
         {
-            Server server = new Server();
-            server.Run();
+            Run();
         }
     }
 }
