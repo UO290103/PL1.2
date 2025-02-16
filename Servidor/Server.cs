3b64772cfb86e8550e02cbbf6f3e7e39fb0fade2
uo287594 <uo287594@uniovi.es>
2025-02-16 10:13:09 +0100

    Final servidor Amp1

--- Servidor/Server.cs
+++ Servidor/Server.cs
@@ -1,117 +1,84 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Net;
 using System.Net.Sockets;
 using Vocabulario;
 
-namespace Server
+namespace Servidor
 {
     public class Server
     {
         private const int _port = 50000;
-        private const int _probFallo = 0;
+        private const int _probFallo = 20;
         private const bool _test = true;
+        static UdpClient client = new UdpClient(_port);
+        static IPEndPoint ip = new IPEndPoint(IPAddress.Any, _port);
+        private static byte[] _ack;
 
         private static void Run()
         {
             // Inicializar el puerto UDP y la dirección IP
-            UdpClient client = new UdpClient(_port);
-            IPEndPoint ip = new IPEndPoint(IPAddress.Any, _port);
-            bool isConnected = false;
 
-            byte[] ack;
-            int seq = 0;
+
 
             // Crear un bloque try-catch para manejar excepciones
             try
             {
-                // Bucle infinito para recibir mensajes del cliente
-                while (true)
+                while (true)  // Bucle principal para el servidor
                 {
-                    // Recibir el mensaje del cliente
+                    Console.WriteLine("Esperando conexión del cliente...");
+
+                    List<int> ints = new List<int>();  // Nueva lista para cada cliente
+                    bool isConnected = false;
+                    int seq = 0;
+
+                    // Esperamos el primer mensaje.
                     byte[] receivedBytes = client.Receive(ref ip);
-
-                    // Verificar conexión
-                    if (!isConnected)
-                    {
-                        Console.WriteLine("Conexión establecida con el cliente.");
-                        isConnected = true;
-                    }
-
-                    // Convertir los datos recibidos a una cadena decodificándolos
                     Data msg = new Data();
                     msg.Decode(receivedBytes);
 
-                    // Aquí almacenaríamos los datos recibidos en un archivo de texto -> No implementado
+                    Console.WriteLine("Conexión establecida con el cliente.");
+                    isConnected = true; // Activamos para entrar en el bucle de transferencia de datos.
 
-                    // Verificar si la secuencia recibida coincide con la secuencia esperada
-                    if (msg.Seq == seq)
+                    seq++; // Aumentamos la secuenci para comenzar a recibir los datos
+
+                    while (isConnected)
                     {
-                        if (msg.Seq == 0)
+                        receivedBytes = client.Receive(ref ip);
+                        msg.Decode(receivedBytes);
+
+                        if (msg.Seq == -1)
                         {
-                            Console.WriteLine("El cliente comienza a transmitir.");
+                            Console.WriteLine("Cliente desconectado. Guardando datos...");
+                            Response(msg.Seq);
+
+                            isConnected = false;
+                            break;  // Salir de este cliente, pero el servidor sigue activo
                         }
 
-                        Console.WriteLine("Secuencia recibida: {0} Mensaje recibido: {1}",
-                            msg.Seq, msg.Number);
-                        seq++;
-
-                        // Si la secuencia es correcta -> Incrementar la secuencia
-                    }
-                    else
-                    {
-                        /*
-                         * Si la secuencia es incorrecta -> No incrementar la secuencia.
-                         * Mostrar el mensaje duplicado y la secuencia esperada.
-                         */
-                        if (_test)
+                        else if (msg.Seq == seq)
                         {
-                            Console.WriteLine("Mensaje duplicado: {0} Secuencia Recibida: {1} Secuencia Esprada: {2}",
-                            msg.Number, msg.Seq, seq);
+                            if (_test)
+                            {
+                                Console.WriteLine($"Seq: {seq} Num: {msg.Number}");
+                            }
+
+                            seq++;
                         }
-                        
+
+
+                        // Ahora hacemos el envio del ACK
+                        Response(msg.Seq);
                     }
-
-
-                    if (msg.Seq != -1)
-                    {
-                        // Crear un mensaje de respuesta para el cliente con la secuencia
-                        ACK response = new ACK(msg.Seq);
-                        ack = response.Encode();
-
-                        // Hacemos que exista la posibilidad de que un ACK no llegue al cliente.
-
-                        var _rand = new Random();
-                        if (_rand.Next(100) > _probFallo)
-                        {
-                            // Enviar el mensaje de reconocimiento al cliente
-                            client.Send(ack, ack.Length, ip);
-                        }
-                        else
-                        {
-                            Console.WriteLine("El ACK se ha perdido.");
-                        }
-                    }
-                    else
-                    {
-                        Console.WriteLine("La comunicación ha finalizado.");
-                        seq = 0;
-                        ACK final = new ACK(-1);
-                        ack = final.Encode();
-                        client.Send(ack, ack.Length, ip);
-                        isConnected = false;
-                    }
-                    
-
-                    // El reconocimiento se envía de vuelta al cliente para confirmar la recepción del mensaje -> No implementado
                 }
             }
             catch (Exception e)
             {
-                Console.WriteLine(e.ToString());
+                Console.WriteLine("Error: " + e.Message);
             }
             finally
             {
-                Console.WriteLine("Conexión terminada.");
+                Console.WriteLine("Servidor apagado.");
                 client.Close();
             }
         }
@@ -119,6 +86,21 @@ namespace Server
         public static void Main()
         {
             Run();
+        }
+
+        public static void Response(int seq)
+        {
+            /*
+             * Este método se emplea para el envio del ACK de manera
+             * que exista la posibilidad de que falle.
+             */
+            ACK res = new ACK(seq);
+            _ack = res.Encode();
+            var rand = new Random();
+            if (rand.Next(100) > _probFallo)
+            {
+                client.Send(_ack, _ack.Length, ip);
+            }
         }
     }
 }
